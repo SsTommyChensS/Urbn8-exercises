285f162d42d31492ede1bd45f86f8178473ae679
Tran Quoc Tung <32635649+SsTommyChensS@users.noreply.github.com>
2022-08-24 17:19:25 +0700

    Update solution a.js

--- exercise-02/a.js
+++ exercise-02/a.js
@@ -1,9 +1,21 @@
-api(function(result){
+function api(api) {
+    api();
+}
+
+function api2(api2) {
+    api2();
+}
+
+function api3(api3) {
+    api3();
+}
+
+api(function(result = 1){
     console.log('result:', result)
-    api2(function(result2) {
-        console.log('result2:', result2)
-        api3(function(result3) {
-            console.log('result3:', result3)
+    api2(function(result2 = 2) {
+        console.log('result2:', result2);
+        api3(function(result3 = 3) {
+            console.log('result2:', result3)
         });
     });
 });
