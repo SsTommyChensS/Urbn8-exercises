250e2e8662a2f3c9a079036c2896aa88845fb8a7
Tran Quoc Tung <32635649+SsTommyChensS@users.noreply.github.com>
2022-08-28 17:01:35 +0700

    Update b.js
    Change the input so that the next values depends on the first value

--- exercise-02/b.js
+++ exercise-02/b.js
@@ -1,32 +1,27 @@
-function api() {
-    return new Promise((resolve) => {
-        resolve(1);
-    })
+function api(api) {
+	return new Promise((resolve) => {
+		resolve(api);
+	})
 }
 
-function api2() {
-    return new Promise((resolve) => {
-        resolve(2);
-    })
+function api2(api2) {
+	return new Promise((resolve) => {
+		resolve(api2);
+	})
 }
 
-function api3() {
-    return new Promise((resolve) => {
-        resolve(3);
-    })
+function api3(api3) {
+	return new Promise((resolve) => {
+		resolve(api3);
+	})
 }
-api().then(function(result){
-    console.log('result:', result);
-    return api2();
-}).then(function(result2){
-    console.log('result2:', result2);
-    return api3();
-}).then(function(result3){
-    console.log('result3:', result3);
-     // do work
-}).catch(function(error) {
-     //handle any error that may occur before this point
-}).then(function() {
-     //do something whether there was an error or not
-     //like hiding an spinner if you were performing an AJAX request.
+
+api(1).then((result) => {
+	console.log('result: ', result);
+	return api2(result + 1);
+}).then((result2) => {
+	console.log('result2: ', result2);
+	return api3(result2 + 1);
+}).then((result3) => {
+	console.log('result3: ', result3);
 });
