e553e2c7eb1c5cca78beda3cde73e1fb897b91f4
Tran Quoc Tung <32635649+SsTommyChensS@users.noreply.github.com>
2022-08-28 17:00:50 +0700

    Update a.js
    Change the input so that the next values depends on the first value

--- exercise-02/a.js
+++ exercise-02/a.js
@@ -11,13 +11,13 @@ function api3(api3) {
 }
 
 api(function(result = 1){
-    console.log('result:', result)
-    api2(function(result2 = 2) {
-        console.log('result2:', result2);
-        api3(function(result3 = 3) {
-            console.log('result2:', result3)
-        });
-    });
+	console.log('result: ', result);
+	api2(function(result2 = result + 1){
+		console.log('result2: ', result2);
+		api3(function(result3 = result2 + 1) {
+			console.log('result3: ', result3);
+		});
+	});
 });
 
 
